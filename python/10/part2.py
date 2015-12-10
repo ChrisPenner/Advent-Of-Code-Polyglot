2c5906879eeb5fd53d5560d7d6f70d5fcd393e0a
Chris Penner <christopher.penner@gmail.com>
2015-12-10 12:00:17 -0600

    Cleaned up the regex a little and added comments, switched to python style snake_case.

--- python/10/part2.py
+++ python/10/part2.py
@@ -1,14 +1,16 @@
 import re
 
-input = '1113222113'
-returningDigits = re.compile('((\\d)(?:\\2|)+)')
+inp = 'XXXXXXXX'
+# This is a little complicated, we find a digit and capture all of its
+# repetitions, in the second capture group we have the single digit itself
+repeating_digits = re.compile(r'((\d)\2*)')
 
 for i in range(0,50):
-    newInput = ''
-    for match, character in returningDigits.findall(input):
-        newInput += str(len(match))
-        newInput += character
+    new_input = ''
+    # Unpack into the total match and the single digit.
+    for match, character in repeating_digits.findall(inp):
+        new_input += str(len(match))
+        new_input += character
+    inp = new_input
 
-    input = newInput
-
-print(len(input))
+print(len(inp))
