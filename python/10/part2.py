b9acb6b82a95e1e00947a286be461035457a7d57
Chris Penner <christopher.penner@gmail.com>
2015-12-09 23:43:05 -0600

    Add python day 10

---
+++ python/10/part2.py
@@ -0,0 +1,27 @@
+inp = 'XXXXXXXX'
+
+# Loop 50 times
+for i in xrange(50):
+    new_sequence = ''
+    # Prime it with the first letter
+    prev_char = inp[0]
+    # Number of the previous char in a row there's been.
+    in_a_row = 1
+    # Loop over chars, we can skip the first one
+    for char in inp[1:]:
+        # Add to the total.
+        if char == prev_char:
+            in_a_row += 1
+            continue
+        # Otherwise we need to put it in the new sequence
+        else:
+            new_sequence += str(in_a_row) + prev_char
+            prev_char = char
+            in_a_row = 1
+    # After the loop is done we need to finish the last number.
+    new_sequence += str(in_a_row) + prev_char
+    prev_char = char
+    in_a_row = 1
+    inp = new_sequence
+
+print len(new_sequence)
