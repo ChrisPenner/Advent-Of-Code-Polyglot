1983436183d49e0b802e25f4ca29f07f9d489bc6
Dave McNeill <dave.mcneill@gmail.com>
2015-12-13 18:56:29 +0000

    add ruby days 4, 6 and 9

---
+++ ruby/04/part1.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'digest'
+
+md5 = Digest::MD5.new
+num_zeros = 5
+input = File.read('input').chomp
+
+0.step do |n|
+  if md5.hexdigest("#{input}#{n.to_s}")[0, num_zeros] == '0' * num_zeros
+    puts n
+    break
+  end
+end
