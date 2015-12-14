c5106a14288e499f2e0e098d3e68779c4e14c406
Chris Penner <christopher.penner@gmail.com>
2015-12-14 15:43:41 -0600

    Add python day 14

---
+++ python/14/part1.py
@@ -0,0 +1,31 @@
+import re
+from operator import itemgetter
+from collections import namedtuple
+
+Reindeer = namedtuple('Reindeer', ['name', 'speed', 'fly_time', 'rest_time', 'cycle_time', 'travel_per_cycle'])
+nums = re.compile(r'\d+')
+reindeer = []
+
+with open('input.txt') as f:
+    for line in f:
+        speed, fly_time, rest_time = map(int, nums.findall(line))
+        name = line.split(' ')[0]
+        cycle_time = fly_time + rest_time
+        travel_per_cycle = fly_time * speed
+        reindeer.append(Reindeer(name, speed, fly_time, rest_time, cycle_time, travel_per_cycle))
+
+def get_scores(time):
+    totals = []
+    for r in reindeer:
+        # Get floor of number of iterations completed, then multiply to get
+        # amount travelled per cycle
+        distance_travelled = (time // r.cycle_time) * r.travel_per_cycle
+        # Get the amount of time unaccounted for
+        leftover = time % r.cycle_time
+        # Get the portion times the travel speed, but max out at fly_time amount
+        distance_travelled += min(leftover * r.speed, r.fly_time * r.speed)
+        totals.append((distance_travelled, r.name))
+    return totals
+
+# Sort by distance then get first element (distance) of highest scoring pair.
+print max(get_scores(2503), key=itemgetter(0))[0]
