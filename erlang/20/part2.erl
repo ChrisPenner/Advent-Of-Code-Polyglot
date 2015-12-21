5b3b49af69c19159018b05731e92f50b1853bf72
LainIwakura <FishLikeHexagon@gmail.com>
2015-12-21 17:12:05 -0500

    Added days 15 to 21

---
+++ erlang/20/part2.erl
@@ -0,0 +1,31 @@
+-module(part2).
+-export([main/0]).
+
+-define(TARGET, 29000000).
+
+main() ->
+	find_house(1).
+
+find_house(N) ->
+	Divs = lists:filter(fun(X) -> N div X =< 50 end, divisors(N)),
+	case (lists:sum(Divs)*11) >= ?TARGET of
+		true ->
+			io:format("~p~n", [N]);
+		false ->
+			find_house(N+1)
+	end.
+
+%% Should be a pretty fast divisors algorithm.
+divisors(0) -> [];
+divisors(1) -> [1];
+divisors(N) -> lists:sort(divisors(1, N)) ++ [N].
+
+divisors(1, N) ->
+	[1] ++ divisors(2, N, math:sqrt(N)).
+divisors(K, _, Q) when K > Q -> [];
+divisors(K, N, _) when N rem K =/= 0 ->
+	[] ++ divisors(K + 1, N, math:sqrt(N));
+divisors(K, N, _) when K * K =:= N ->
+	[K] ++ divisors(K + 1, N, math:sqrt(N));
+divisors(K, N, _) ->
+	[K, N div K] ++ divisors(K + 1, N, math:sqrt(N)).
---
+++ erlang/21/part1.erl
@@ -0,0 +1,75 @@
+-module(part1).
+-export([main/0]).
+
+-define(WEAPONS, [
+	{dagger,     [{cost,  8},{dam,4},{def,0}]}, 
+	{shortsword, [{cost, 10},{dam,5},{def,0}]}, 
+	{warhammer,  [{cost, 25},{dam,6},{def,0}]}, 
+	{longsword,  [{cost, 40},{dam,7},{def,0}]}, 
+	{greataxe,   [{cost, 74},{dam,8},{def,0}]}
+]).
+
+-define(ARMOR, [
+	{dummy,      [{cost,   0},{dam,0},{def,0}]},
+	{leather,    [{cost,  13},{dam,0},{def,1}]}, 
+	{chainmail,  [{cost,  31},{dam,0},{def,2}]}, 
+	{splintmail, [{cost,  53},{dam,0},{def,3}]}, 
+	{bandedmail, [{cost,  75},{dam,0},{def,4}]}, 
+	{platemail,  [{cost, 102},{dam,0},{def,5}]}
+]).
+
+-define(RINGS, [
+	{dam1, [{cost, 25},{dam,1},{def,0}]}, 
+	{dam2, [{cost, 50},{dam,2},{def,0}]}, 
+	{dam3, [{cost,100},{dam,3},{def,0}]}, 
+	{def1, [{cost, 20},{dam,0},{def,1}]}, 
+	{def2, [{cost, 40},{dam,0},{def,2}]},
+	{def3, [{cost, 80},{dam,0},{def,3}]},
+	{dum1, [{cost, 0},{dam,0},{def,0}]},
+	{dum2, [{cost, 0},{dam,0},{def,0}]}
+]).
+
+-define(PLAYER, [{hp,100},{dam,0},{def,0}]).
+-define(BOSS, [{hp,103},{dam,9},{def,2}]).
+
+main() ->
+	Builds = get_builds(),
+	WinningBuild = lists:min(find_winners(Builds, [])),
+	io:format("~p~n", [WinningBuild]).
+
+get_builds() ->
+	[{W,A,R1,R2} || 
+	 W <- ?WEAPONS, 
+	 A <- ?ARMOR, 
+	 R1 <- ?RINGS, 
+	 R2 <- ?RINGS, 
+	 R1 /= R2].
+
+find_winners([], Acc) ->
+	Acc;
+find_winners([H|T], Acc) ->
+	Build = tuple_to_list(H),
+	Cost = lists:foldl(fun(X, Sum) -> {_, Prop} = X, Sum + proplists:get_value(cost, Prop) end, 0, Build),
+	DamBoost = lists:foldl(fun(X, Sum) -> {_, Prop} = X, Sum + proplists:get_value(dam, Prop) end, 0, Build),
+	DefBoost = lists:foldl(fun(X, Sum) -> {_, Prop} = X, Sum + proplists:get_value(def, Prop) end, 0, Build),
+	PDam = proplists:get_value(dam, ?PLAYER) + DamBoost,
+	PDef = proplists:get_value(def, ?PLAYER) + DefBoost,
+	BDam = proplists:get_value(dam, ?BOSS),
+	BDef = proplists:get_value(def, ?BOSS),
+	BMoves = ceiling(proplists:get_value(hp, ?PLAYER) / max((BDam - PDef), 1)),
+	PMoves = ceiling(proplists:get_value(hp, ?BOSS) / max((PDam - BDef), 1)),
+	case PMoves =< BMoves of
+		true ->
+			find_winners(T, [Cost|Acc]);
+		false ->
+			find_winners(T, Acc)
+	end.
+
+ceiling(X) when X < 0 ->
+	trunc(X);
+ceiling(X) ->
+	T = trunc(X),
+	case X - T == 0 of
+		true -> T;
+		false -> T + 1
+	end.
