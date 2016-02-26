37874ac2fbccb51e7db2f27936230d936b408672
voddan <dgvodopyan@gmail.com>
2016-02-26 21:32:50 +0300

    Added day 02 (part 1, 2)

---
+++ kotlin/02/part1.kt
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val input = File("input.txt").readLines()
+
+    val res = input.sumBy { str ->
+        val (l, w, h) = str.split('x').map {it.toInt()}
+
+        val a = l * w
+        val b = l * h
+        val c = w * h
+
+        val min = listOf(a, b, c).min()!!
+
+        2 * (a + b + c) + min
+    }
+
+    println(res)
+}
