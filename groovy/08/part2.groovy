844175ddfda55f13d8837081dc1b2f95b588697a
Youri Ackx <youri@ackx.net>
2016-08-06 12:29:02 +0200

    Only count extra characters for part2

--- groovy/08/part2.groovy
+++ groovy/08/part2.groovy
@@ -2,14 +2,8 @@
 
 int total = 0
 new File('input.txt').each { line ->
-	def count = 2	// opening+closing double quotes
-	line.eachWithIndex { c, i ->
-		if (c == '\\' || c == '"') {
-			count++
-		}
-		count++
-	}
-	total = total + count - line.size()
+	def extra = 2 + line.count('\\') + line.count('"')
+	total = total + extra
 }
 
 println total
