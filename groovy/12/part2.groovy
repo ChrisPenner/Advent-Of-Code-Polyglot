98297d439c0d7dd67b29b9daddd06e386be40276
Youri Ackx <youri@ackx.net>
2016-08-09 17:19:08 +0200

    - Return branch sum i.o. relying on external total - Replace if/else construct by if/return (go-lang style)

--- groovy/12/part2.groovy
+++ groovy/12/part2.groovy
@@ -1,19 +1,19 @@
 import groovy.json.JsonSlurper;
 
-total = 0
-
-def add(o) {
+int add(o) {
     if (o instanceof Map) {
         // JSON object
-        if (o.values().contains('red')) return
-        o.each { k, v -> add v }
-    } else if (o instanceof ArrayList) {
-        // JSON array
-        o.each { add it }
-    } else {
-        // JSON value + is numeric
-        if (o instanceof Integer) total += o
+        if (o.values().contains('red')) return 0
+        return o.collect { k, v -> add v }.sum()
     }
+    if (o instanceof ArrayList) {
+        // JSON array
+        return o.collect { add it }.sum()
+    }
+    // JSON value + is numeric
+    if (o instanceof Integer) return o
+
+    return 0    // String
 }
 
 // Read input as a string
@@ -23,5 +23,4 @@ def json = new File('input.txt').text
 def jsonObject = new JsonSlurper().parseText(json)
 
 // Recursively add numbers and print total
-add jsonObject
-println total
+println add(jsonObject)
