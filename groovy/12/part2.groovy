8125b488a5f07fa29006abe37ec9bc04d53efca0
Youri Ackx <youri@ackx.net>
2016-08-09 17:23:15 +0200

    Different layout

--- groovy/12/part2.groovy
+++ groovy/12/part2.groovy
@@ -1,19 +1,18 @@
 import groovy.json.JsonSlurper;
 
 int add(o) {
-    if (o instanceof Map) {
-        // JSON object
+    if (o instanceof Map) {                 // JSON object
         if (o.values().contains('red')) return 0
         return o.collect { k, v -> add v }.sum()
     }
-    if (o instanceof ArrayList) {
-        // JSON array
+
+    if (o instanceof ArrayList) {           // JSON array
         return o.collect { add it }.sum()
     }
-    // JSON value + is numeric
-    if (o instanceof Integer) return o
 
-    return 0    // String
+    if (o instanceof Integer) return o      // JSON value (numeric)
+
+    return 0                                // String
 }
 
 def json = new File('input.txt').text
