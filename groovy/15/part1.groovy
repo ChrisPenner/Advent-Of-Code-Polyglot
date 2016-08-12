ce6d5be916bb84cbcd4b318a028d083c0dccca5c
Youri Ackx <youri@ackx.net>
2016-08-12 17:32:01 +0200

    Day 15 - groovy - part 1

---
+++ groovy/15/part1.groovy
@@ -0,0 +1,41 @@
+#!/usr/bin/env groovy
+
+// Read ingredients and store numeric characteristics
+def lines = new File('input.txt') as String []
+def rNumber = ~/-?\d+/
+def ingredients = lines.collect({ it.findAll(rNumber)*.toInteger() })
+
+def highScore = 0
+
+// Combine the given ingredient proportions and test score
+def combine = { proportions ->
+    def score = (0..3).collect { property ->
+        def pScore = 0
+        ingredients.eachWithIndex { ingredient, i ->
+            pScore += ingredient[property] * proportions[i]
+        }
+        pScore > 0 ? pScore : 0
+    }.inject(1) { product, item -> product*item } // mult the properties scores
+
+    if (score > highScore) {
+        highScore = score
+    }
+}
+
+// Try all possible proportions of ingredients
+def mix
+mix = { proportions ->
+    def left = 100 - (proportions.sum() ?: 0)
+    if (proportions.size()+1 == ingredients.size()) {
+        combine(proportions + [left])
+    } else {
+        (0..left).each {
+            def that = proportions + [it]
+            mix(proportions + [it])
+        }
+    }
+}
+
+mix([] as List<Integer>)
+
+println highScore
