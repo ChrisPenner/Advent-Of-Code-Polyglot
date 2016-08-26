52060dc1ca7057def901a73095eef199d3f2bd27
Youri Ackx <youri@ackx.net>
2016-08-26 16:59:32 +0200

    Align

--- groovy/17/part1.groovy
+++ groovy/17/part1.groovy
@@ -18,7 +18,7 @@ references regardeless of their 'foo' capacity.
 Examples:
 
 def foo = [1, 1, 1, 2]
-assert 7 == foo.subsequences().size()   // not what we want!
+assert 7 == foo.subsequences().size()    // not what we want!
 def bar = [1, 1, 1, 2].collect({ new Expando(foo: it) })
 assert 15 == bar.subsequences().size()   // that's better
 */
